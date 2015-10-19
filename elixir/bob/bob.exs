a4a0dbda1739e1b77d8dbc4b447e54387d688f8f
Ryan Linton <ryanl@clearsightstudio.com>
2015-10-19 21:18:56 +0900

    Solve bob in elixir

--- elixir/bob/bob.exs
+++ elixir/bob/bob.exs
@@ -1,8 +1,15 @@
 defmodule Teenager do
   def hey(input) do
+    input = String.strip(input)
     cond do
-      true -> "Whatever."
-
+      String.length(input) == 0 ->
+        "Fine. Be that way!"
+      String.ends_with?(input, "?") ->
+        "Sure."
+      String.match?(input, ~r/\p{Lu}/) and String.upcase(input) == input ->
+        "Whoa, chill out!"
+      true ->
+        "Whatever."
     end
   end
 end
